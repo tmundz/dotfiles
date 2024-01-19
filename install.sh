082b5fc5d83840b7a12a76897c087222682fb71f
Travis <mundy.travis@gmail.com>
2024-01-19 16:22:07 -0700

    Update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-#install rust 
-#install paru 
-# install general pacman repos 
-# Choose wm sway hyprland xmonad etc 
-#install pacman repos
+# Define the file containing the list of packages
+PACKAGE_LIST="packages.txt"
 
-# Move config files 
+# Check if paru is installed
+if ! command -v paru &> /dev/null
+then
+    echo "paru could not be found, please install paru first."
+    exit 1
+fi
+
+# Install each package from the list
+while IFS= read -r package
+do
+    # Extract only the package name (before the first space)
+    pkg_name=$(echo "$package" | cut -d ' ' -f 1)
+    
+    # Install the package using paru
+    echo "Installing $pkg_name..."
+    paru -S --noconfirm "$pkg_name"
+done < "$PACKAGE_LIST"
+
+echo "All packages have been installed."
+
