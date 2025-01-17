bc92f90bed33f492637185c6ac96cc0d150b775b
tmundz <mundy.travis@outlook.com>
2025-01-17 15:32:19 -0700

    Pushing the new install script and making sure rofi is in there

--- install-2.sh
+++ install-2.sh
@@ -24,28 +24,16 @@ echo "Installing Dev tools"
 sudo pacman -S brightnessctl zsh udiskie go delve gdb make cmake gcc man-db man-pages python gopls zig neovim
 
 echo "Installing Hyprland"
-paru -S pcmanfm docker mullvad-vpn grim slurp mako kitty hyprland rofi-wayland fastfetch mpv wlogout hypridle hyprlock hyprpicker xdg-desktop-portal-hyprland hyprcursor wireplumber qt5-wayland qt6-wayland waybar copyq wl-clipboard hyprpaper swaybg kde-polkit-agent
+paru -S bat mullvad-vpn grim slurp mako kitty hyprland rofi-wayland fastfetch mpv wlogout hypridle hyprlock hyprpicker xdg-desktop-portal-hyprland hyprcursor wireplumber qt5-wayland qt6-wayland waybar copyq wl-clipboard hyprpaper swaybg kde-polkit-agent
 
 echo "Installing software"
-paru -S ntfs-3g keepassxc openssh rofi-power-menu feh rofi-wifi-menu candy-icons cava ranger gimp kdenlive krita zen-browser firefox uwufetch fzf qbittorrent ripgrep lazygit zathura obs-studio
+paru -S ntfs-3g keepassxc openssh rofi-power-menu feh rofi-wifi-menu candy-icons cava ranger gimp kdenlive krita brave-bin firefox uwufetch fzf qbittorrent ripgrep lazygit thunar zathura obs-studio
 
 echo "Installing Fonts"
 paru -S nerd-fonts adobe-source-code-pro-fonts cantarell-fonts fontconfig fonts-cjk gnu-free-fonts libfontenc libxfont2 ttf-font-awesome ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-nerd-fonts-symbols-mono ttf-profont-nerd xorg-fonts-encodings xorg-mkfontscale ttf-hack
 
 echo "Installing KVM"
 sudo pacman -S qemu-full virt-manager virt-viewer dnsmasq bridge-utils libguestfs ebtables vde2 openbsd-netcat
-
-echo "Installing the needed images"
-cd ~
-git clone --depth=1 https://github.com/adi1090x/rofi.git
-cd rofi
-chmod +x setup.sh
-./setup.sh
-cd ~
-
-echo "Adding BB Tools"
-paru -S caido hashcat scrcpy wireshark-qt gobuster ffuf binwalk ghidra radare2 android-tools android-studio burpsuite android-emulator android-apktool jadx
-
 
 echo "Moving Configs"
 cp ~/dotfiles/.zshrc ~/.zshrc
@@ -63,27 +51,16 @@ cp -r ~/dotfiles/.config/kitty ~/.config/
 cp -r ~/dotfiles/.config/cava ~/.config/
 cp -r ~/dotfiles/.config/lazygit ~/.config/
 cp -r ~/dotfiles/.config/copyq ~/.config/
-cp -r ~/dotfiles/.themes/ ~/
-
-
 
 echo "Enabling services"
 sudo systemctl enable sshd
 sudo systemctl start sshd
-
-sudo systemctl start docker                                        
-sudo systemctl enable docker
 
 sudo systemctl enable libvirtd.service
 sudo systemctl start libvirtd.service
 
 sudo usermod -aG video,ftp,log,uucp,tty,utmp,kvm,input,audio,storage $USER
 sudo usermod -s /bin/zsh $USER
-
-virsh net-start default
-virsh net-autostart default
-
-sudo usermod -aG docker $USER
 
 echo "Git global UserName type Leave blank to skip"
 read username
@@ -94,17 +71,8 @@ if [[ -n ${username} ]]; then
   git config --global user.email ${email}
 fi
 
-echo "Pulling docker mobsf"
-sudo docker pull opensecurity/mobile-security-framework-mobsf:latest
-
-echo "[+] to run mobfs"
-echo "[+] docker run -it --rm -p 8000:8000 opensecurity/mobile-security-framework-mobsf:latest"
-
-#bat --theme="Catppuccin Mocha" ~/.config/bat/themes/Catppuccin\ Mocha.tmTheme
-echo "[+] GO TO /etc/libvirt/libvirtd.conf and uncomment "
-echo '[+] unix_sock_group = "libvirt" unix_sock_rw_perms = "0777"'
+bat --theme="Catppuccin Mocha" ~/.config/bat/themes/Catppuccin\ Mocha.tmTheme
+echo "GO TO /etc/libvirt/libvirtd.conf and uncomment "
+echo 'unix_sock_group = "libvirt" unix_sock_rw_perms = "0777"'
 echo "sudo usermod -aG libvirt $USER run this"
-sudo usermod -aG libvirt $USER
 
-# will eventually connect to my prox mox server but I do eventually want to re vamp this
-# fix thjis up 
